894710d5117b45e31e0461735e93db02dafd131a
vishal <vishalsinghvicky95@gmail.com>
2025-07-24 23:56:08 +0530

    server update

--- Backend/server.js
+++ Backend/server.js
@@ -45,5 +45,9 @@ app.use("/api/messages", messageRouter);
 // Connection to mongoDB
 await connectDB();
 
+
+if(process.env.NODE_ENV !=="production"){
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, ()=>console.log("Server is running on PORT:" + PORT));
+}
+export default server;
